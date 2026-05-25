7ba56d5c7aef63d2250768f6441e685dc309de25
VietHoang <hoangdv955@gmail.com>
2026-05-25 20:15:52 +0700

    fix momo

--- backend/src/config/momo.js
+++ backend/src/config/momo.js
@@ -1,14 +1,15 @@
 const crypto = require('crypto');
+require('dotenv').config(); // Khai báo dòng này để đảm bảo Node.js đọc được file .env
 
-// BỘ KEY TEST CHUẨN TỪ GITHUB CỦA MOMO DEVELOPERS
+// BỘ KEY TEST CHUẨN ĐỌC TỪ FILE .ENV (DÙNG CHUNG CHO CẢ TEAM)
 const momoConfig = {
-  partnerCode: 'MOMOBKUN20180529',
-  accessKey: 'klm05TvNBzhg7h7j', // Đã sửa lại đúng key
-  secretKey: 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa', // Đã sửa lại đúng key
+  partnerCode: process.env.MOMO_PARTNER_CODE,
+  accessKey: process.env.MOMO_ACCESS_KEY,
+  secretKey: process.env.MOMO_SECRET_KEY,
   
-  apiUrl: 'https://test-payment.momo.vn/v2/gateway/api/create',
-  redirectUrl: 'http://localhost:5173/payment/result',
-  ipnUrl: 'https://staff-herald-granite.ngrok-free.dev/api/v1/payments/webhooks/momo' 
+  apiUrl: process.env.MOMO_API_URL,
+  redirectUrl: process.env.MOMO_REDIRECT_URL,
+  ipnUrl: process.env.MOMO_IPN_URL 
 };
 
 // Hàm tạo chữ ký bảo mật HMAC SHA256
