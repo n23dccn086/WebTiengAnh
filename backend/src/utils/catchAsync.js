5b5b2fe04ffe827a680cdaa06245da5f5dd7efc8
Nguyen Le Nhut Hao <n23dccn086@student.ptithcm.edu.vn>
2026-06-01 14:01:57 +0700

    delete empty files and folders

--- backend/src/utils/catchAsync.js
+++ backend/src/utils/catchAsync.js
@@ -9,18 +9,3 @@ const catchAsync = (fn) => {
 module.exports = catchAsync;
 
 
-//  Thay vì viết thế này:
-// exports.register = async (req, res, next) => {
-//   try {
-//      const user = await authService.register(req.body);
-//      res.status(201).json({ status: 'success' });
-//   } catch (err) {
-//      next(err);
-//   }
-// }
-
-//  Bạn chỉ cần viết thế này nhờ catchAsync:
-// exports.register = catchAsync(async (req, res, next) => {
-//   const user = await authService.register(req.body);
-//   res.status(201).json({ status: 'success' });
-// });
